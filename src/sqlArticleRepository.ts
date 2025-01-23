b3aaedeaccf87e0ea499a3fd867d4eef9b75302d
Mateusz Kwasniewski <mateusz.kwasniewski@bottega.com.pl>
2025-01-23 16:56:16 +0100

    sql

--- src/sqlArticleRepository.ts
+++ src/sqlArticleRepository.ts
@@ -14,10 +14,23 @@ export const sqlArticleRepository = (db: Kysely<DB>): ArticleRepository => {
             }
         },
         async update(article) {
-            await db.updateTable('article').set(article).where('article.id', '=', article.id).execute();
+            await db.updateTable('article as a').set(article).where('a.id', '=', article.id).execute();
+            await db.deleteFrom('tags').where('articleId', '=', article.id).execute();
+            if (article.tagList.length > 0) {
+                await db.insertInto('tags').values(article.tagList.map(tag => ({
+                    name: tag,
+                    articleId: article.id
+                }))).execute();
+            }
         },
         async findBySlug(slug) {
-            return null;
+            const article = await db.selectFrom('article').where('slug', '=', slug).selectAll().executeTakeFirst();
+            if(!article) return null;
+            const tags = await db.selectFrom('tags').where('articleId', '=', article.id).selectAll().execute();
+            return {
+                ...article,
+                tagList: tags.map(tag => tag.name)
+            };
         },
     };
 };
