78269f24a075cd938dade780b55c2aaaa4324918
Maciej Zych <maciej.zych@paramount.com>
2025-01-23 12:03:03 +0100

    step 1

---
+++ src/articleRouter.ts
@@ -0,0 +1,71 @@
+import express from "express";
+
+import makeSlug from "slug";
+import omit from "lodash.omit";
+import merge from "lodash.merge";
+import { incrementIdGenerator } from "./incrementIdGenerator";
+import { NotFoundError } from "./NotFoundError";
+
+export const articleRouter = express();
+
+type Article = {
+  body: string;
+  description: string;
+  tagList: Array<string>;
+  title: string;
+  slug: string;
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+const articleIdGenerator = incrementIdGenerator(String);
+const articles: Record<string, Article> = {};
+
+articleRouter.post("/api/articles", async (req, res, next) => {
+  const input = req.body.article;
+
+  const now = new Date();
+  const article: Article = {
+    body: input.body,
+    description: input.description,
+    tagList: input.tagList,
+    title: input.title,
+    slug: makeSlug(input.title),
+    id: articleIdGenerator(),
+    createdAt: now,
+    updatedAt: now,
+  };
+  articles[article.id] = article;
+
+  res.json({ article: omit(article, "id") });
+});
+
+articleRouter.put("/api/articles/:slug", async (req, res, next) => {
+  const articleInput = req.body.article;
+  const slug = req.params.slug;
+  const existingArticle = Object.values(articles).find(
+    (article) => article.slug === slug
+  );
+  if (!existingArticle) {
+    throw new NotFoundError(`Article with slug ${slug} does not exist`);
+  }
+  const article = merge(existingArticle, articleInput);
+  const now = new Date();
+  article.updatedAt = now;
+  article.slug = makeSlug(article.title);
+
+  articles[article.id] = article;
+  res.json({ article: omit(article, "id") });
+});
+
+articleRouter.get("/api/articles/:slug", async (req, res, next) => {
+  const slug = req.params.slug;
+
+  const existingArticle = Object.values(articles).find(
+    (article) => article.slug === slug
+  );
+  if (!existingArticle) {
+    throw new NotFoundError(`Article with slug ${slug} does not exist`);
+  }
+  res.json({ article: omit(existingArticle, "id") });
+});
