c243e1db13dbf6374f34d6db6e52f7edc46faf7e
Mateusz Kwasniewski <mateusz.kwasniewski@bottega.com.pl>
2025-01-23 11:44:47 +0100

    articles router

--- src/app.ts
+++ src/app.ts
@@ -1,76 +1,13 @@
-import express, { ErrorRequestHandler } from "express";
+import express, {ErrorRequestHandler} from "express";
 import cors from "cors";
-import { NotFoundError } from "./NotFoundError";
-import makeSlug from "slug";
-import { incrementIdGenerator } from "./incrementIdGenerator";
-import omit from "lodash.omit";
-import merge from "lodash.merge";
-
-type Article = {
-  body: string;
-  description: string;
-  tagList: Array<string>;
-  title: string;
-  slug: string;
-  id: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
-const articleIdGenerator = incrementIdGenerator(String);
-const articles: Record<string, Article> = {};
+import {NotFoundError} from "./NotFoundError";
+import {articlesRouter} from "./articlesRouter";
 
 export const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/api/articles", async (req, res, next) => {
-  const input = req.body.article;
-
-  const now = new Date();
-  const article: Article = {
-    body: input.body,
-    description: input.description,
-    tagList: input.tagList,
-    title: input.title,
-    slug: makeSlug(input.title),
-    id: articleIdGenerator(),
-    createdAt: now,
-    updatedAt: now,
-  };
-  articles[article.id] = article;
-
-  res.json({ article: omit(article, "id") });
-});
-
-app.put("/api/articles/:slug", async (req, res, next) => {
-  const articleInput = req.body.article;
-  const slug = req.params.slug;
-  const existingArticle = Object.values(articles).find(
-    (article) => article.slug === slug,
-  );
-  if (!existingArticle) {
-    throw new NotFoundError(`Article with slug ${slug} does not exist`);
-  }
-  const article = merge(existingArticle, articleInput);
-  const now = new Date();
-  article.updatedAt = now;
-  article.slug = makeSlug(article.title);
-
-  articles[article.id] = article;
-  res.json({ article: omit(article, "id") });
-});
-
-app.get("/api/articles/:slug", async (req, res, next) => {
-  const slug = req.params.slug;
-
-  const existingArticle = Object.values(articles).find(
-    (article) => article.slug === slug,
-  );
-  if (!existingArticle) {
-    throw new NotFoundError(`Article with slug ${slug} does not exist`);
-  }
-  res.json({ article: omit(existingArticle, "id") });
-});
+app.use(articlesRouter);
 
 app.use((req, res, next) => {
   throw new NotFoundError();
